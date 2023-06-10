606369b60e89e0a287b1e5beaa61dc7fe46fa7e1
You Song Hou <ysongweb3@gmail.com>
2023-06-10 01:11:54 -0400

    feat: install and add React Native Elements

--- screens/Home.js
+++ screens/Home.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button } from '@rneui/themed';
 
 export default function Home() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>P2P MobileChat</Text>
+      <Button title="Login" />
       <StatusBar style="auto" />
     </View>
   );
