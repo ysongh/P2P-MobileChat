79e2edca4e1efc671eb969fcfbcb42a6a9c13d5a
You Song Hou <ysongweb3@gmail.com>
2023-06-14 22:13:03 -0400

    feat: add chat messages

--- screens/Chat.js
+++ screens/Chat.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, SafeAreaView, KeyboardAvoidingView, Platform, ScrollView, View, TextInput, TouchableOpacity } from 'react-native';
-import { Button, Text } from '@rneui/themed';
+import { StyleSheet, SafeAreaView, KeyboardAvoidingView, Platform, ScrollView, View, TextInput, TouchableOpacity, Keyboard } from 'react-native';
+import { Avatar, Text } from '@rneui/themed';
 
 import { getChatsFromPB, addChatToPB } from '../utils/polybase';
 
@@ -12,21 +12,61 @@ export default function Chat() {
     getChatsFromPB();
   }, [])
 
+  const sendMessage = () => {
+    Keyboard.dismiss();
+    addChatToPB("2", "0x0", "0x0", input);
+    setInput("");
+  }
+
   return (
     <SafeAreaView style={{  flex: 1, backgroundColor: '#fff'}}>
       <StatusBar style="auto" />
 
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding": "height"}
+        style={styles.container}
         keyboardVerticalOffset={90}
       >
         <>
-          <ScrollView>
-
+          <ScrollView contentContainerStyle={{ paddingTop: 15 }}>
+            <View style={styles.reciever}>
+              <Avatar
+                rounded
+                position="absolute"
+                bottom={-15}
+                right={-5}
+                size={30}
+                title="Fc"
+                containerStyle={{
+                  position: "absolute",
+                  bottom: -15,
+                  right: -5,
+                  backgroundColor: "green"
+                }} />
+              <Text style={styles.recieverText}>Hi</Text>
+            </View>
+            <View style={styles.sender}>
+              <Avatar
+                rounded
+                position="absolute"
+                bottom={-15}
+                left={-5}
+                size={30}
+                title="CJ"
+                containerStyle={{
+                  position: "absolute",
+                  bottom: -15,
+                  left: -5,
+                  backgroundColor: "blue"
+                }} />
+              <Text style={styles.senderText}>Hi</Text>
+              <Text style={styles.senderName}>0x0</Text>
+            </View>
+            
           </ScrollView>
           <View style={styles.footer}>
-            <TextInput value={input} onChange={(text) => setInput(text)} placeholder="msg..."/>
-            <TouchableOpacity onPress={() => addChatToPB("2", "0x0", "0x0", input)}>
+            <TextInput style={styles.textInput} value={input} onChange={(text) => setInput(text)} placeholder="msg..." />
+            <TouchableOpacity onPress={sendMessage}>
               <Text>Send</Text>
             </TouchableOpacity>
           </View>
@@ -42,9 +82,57 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
   },
+  reciever: {
+    padding: 15,
+    backgroundColor: "#E9E4F0",
+    alignSelf: "flex-end",
+    borderRadius: 20,
+    marginRight: 15,
+    marginBottom: 20,
+    maxWidth: "80%",
+    position: "relative"
+  },
+  recieverText: {
+    color: "black",
+    fontWeight: "500",
+    marginLeft: 10,
+  },
+  sender: {
+    padding: 15,
+    backgroundColor: "#7F3FBF",
+    alignSelf: "flex-start",
+    borderRadius: 20,
+    margin: 15,
+    maxWidth: "80%",
+    position: "relative"
+  },
+  senderText: {
+    color: "white",
+    fontWeight: "500",
+    marginLeft: 10,
+    marginBottom: 15
+  },
+  senderName: {
+    left: 10,
+    paddingRight: 10,
+    fontSize: 10,
+    color: "white"
+  },
+  textInput: {
+    bottom: 0,
+    height: 40,
+    flex: 1,
+    marginRight: 10,
+    borderColor: "transparent",
+    borderRadius: 30,
+    padding: 10,
+    backgroundColor: "#E9E4F0",
+    color: "grey"
+  },
   footer: {
     flexDirection: "row",
     alignItems: "center",
-    width: "100%"
+    width: "100%",
+    padding: 10
   }
 });
